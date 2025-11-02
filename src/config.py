f80340022e136bb78bdd4bcbd4d8bf6b07a4bf8d
anghoo <leeanghoo2@gmail.com>
2025-11-03 00:01:48 +0900

    refactor: implement environment variable passthrough for database configuration (#2)

--- src/config.py
+++ src/config.py
@@ -1,5 +1,22 @@
+import os
+
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from enum import StrEnum
+
+
+class Environ(StrEnum):
+    PRD = "PRD"
+    DEV = "DEV"
+
+
+def get_env_file_name(env_key: str) -> str:
+    env_key_normalized = env_key if env_key.upper() in Environ else Environ.DEV
+    env_file_mapper: dict[Environ, str] = {
+        Environ.PRD: ".env",
+        Environ.DEV: ".env.dev",
+    }
+    return env_file_mapper.get(env_key_normalized, ".env.dev")
 
 
 class DatabaseConfig(BaseSettings):
@@ -8,6 +25,10 @@ class DatabaseConfig(BaseSettings):
         default="SQLALCHEMY_DATABASE_URL", alias="SQLALCHEMY_DATABASE_URL"
     )
     echo: bool = Field(default=True, alias="DATABASE_ECHO")
+    model_config = SettingsConfigDict(
+        env_file=get_env_file_name(os.getenv("CONF_ENV")),
+        env_file_encoding="utf-8",
+    )
 
 
 class CORSConfig(BaseSettings):
