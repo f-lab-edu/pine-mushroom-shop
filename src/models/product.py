48b70e35feca73d5605de34edf4663263edcf737
anghoo <leeanghoo2@gmail.com>
2025-11-02 22:38:22 +0900

    refactor: remove unused import module (#2)

--- src/models/product.py
+++ src/models/product.py
@@ -14,7 +14,6 @@ from sqlalchemy import (
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
-from src.models.orders import OrderProduct
 
 if TYPE_CHECKING:
     from src.models.orders import OrderProduct
